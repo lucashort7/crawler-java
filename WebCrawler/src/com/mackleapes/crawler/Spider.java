8e1629f5af70435170d9ea7e12bdd7daf2a8994d
Lucas Hort <lucashort7@gmail.com>
2017-12-01 16:18:09 -0200

    Search method

--- WebCrawler/src/com/mackleapes/crawler/Spider.java
+++ WebCrawler/src/com/mackleapes/crawler/Spider.java
@@ -30,6 +30,31 @@ public class Spider {
     private List<String> pagesToVisit = new LinkedList<String>();
 
     
+    public void search(String url, String searchWord){
+        while(this.pagesVisited.size() < MAX_PAGES_TO_SEARCH)
+        {
+            String currentUrl;
+            SpiderLeg leg = new SpiderLeg();
+            if(this.pagesToVisit.isEmpty()){
+                currentUrl = url;
+                this.pagesVisited.add(url);
+            }
+            else{
+                currentUrl = this.nextUrl();
+            }
+            leg.crawl(currentUrl); // Lots of stuff happening here. Look at the crawl method in
+                                   // SpiderLeg
+            boolean success = leg.searchForWord(searchWord);
+            if(success){
+                System.out.println(String.format("**Success** Word %s found at %s", searchWord, currentUrl));
+                break;
+            }
+            this.pagesToVisit.addAll(leg.getLinks());
+        }
+        System.out.println(String.format("**Done** Visited %s web page(s)", this.pagesVisited.size()));
+    }    
+    
+    
     private String nextUrl() {
         String nextUrl;
 
--- WebCrawler/src/com/mackleapes/crawler/SpiderLeg.java
+++ WebCrawler/src/com/mackleapes/crawler/SpiderLeg.java
@@ -5,10 +5,23 @@
  */
 package com.mackleapes.crawler;
 
+import java.util.List;
+
 /**
  *
  * @author Lucas Hort
  */
 public class SpiderLeg {
+    
+    // Give it a URL and it makes an HTTP request for a web page
+    public void crawl(String nextURL){
+    } 
+     // Tries to find a word on the page
+    public boolean searchForWord(String word){
+        
+    }
+    // Returns a list of all the URLs on the page
+    public List<String> getLinks() {
+    }
     
 }
