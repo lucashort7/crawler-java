0a677fff1c542a2ffe17300573f6663695e82696
Lucas Hort <lucashort7@gmail.com>
2017-12-01 17:03:43 -0200

    Proxy

--- WebCrawler/src/com/mackleapes/crawler/Spider.java
+++ WebCrawler/src/com/mackleapes/crawler/Spider.java
@@ -10,60 +10,56 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
-/**
- *
- * @author Lucas Hort start : 01/12
- *
- * - REQUISITS - Retrieve a web page (we'll call it a document) from a website
- * Collect all the links on that document Collect all the words on that document
- * See if the word we're looking for is contained in the list of words Visit the
- * next link Keep track of pages that we've already visited Put a limit on the
- * number of pages to search so this doesn't run for eternity.
- *
- *
- */
 public class Spider {
 
-    // Fields
     private static final int MAX_PAGES_TO_SEARCH = 10;
     private Set<String> pagesVisited = new HashSet<String>();
     private List<String> pagesToVisit = new LinkedList<String>();
 
-    
-    public void search(String url, String searchWord){
-        while(this.pagesVisited.size() < MAX_PAGES_TO_SEARCH)
-        {
+    /**
+     * Our main launching point for the Spider's functionality. Internally it
+     * creates spider legs that make an HTTP request and parse the response (the
+     * web page).
+     *
+     * @param url - The starting point of the spider
+     * @param searchWord - The word or string that you are searching for
+     */
+    public void search(String url, String searchWord) {
+        while (this.pagesVisited.size() < MAX_PAGES_TO_SEARCH) {
             String currentUrl;
             SpiderLeg leg = new SpiderLeg();
-            if(this.pagesToVisit.isEmpty()){
+            if (this.pagesToVisit.isEmpty()) {
                 currentUrl = url;
                 this.pagesVisited.add(url);
-            }
-            else{
+            } else {
                 currentUrl = this.nextUrl();
             }
+            System.out.println("BATATA 1");
             leg.crawl(currentUrl); // Lots of stuff happening here. Look at the crawl method in
-                                   // SpiderLeg
+            // SpiderLeg
             boolean success = leg.searchForWord(searchWord);
-            if(success){
+            if (success) {
                 System.out.println(String.format("**Success** Word %s found at %s", searchWord, currentUrl));
                 break;
             }
             this.pagesToVisit.addAll(leg.getLinks());
         }
-        System.out.println(String.format("**Done** Visited %s web page(s)", this.pagesVisited.size()));
-    }    
-    
-    
+        System.out.println("\n**Done** Visited " + this.pagesVisited.size() + " web page(s)");
+    }
+
+    /**
+     * Returns the next URL to visit (in the order that they were found). We
+     * also do a check to make sure this method doesn't return a URL that has
+     * already been visited.
+     *
+     * @return
+     */
     private String nextUrl() {
         String nextUrl;
-
         do {
             nextUrl = this.pagesToVisit.remove(0);
         } while (this.pagesVisited.contains(nextUrl));
-
         this.pagesVisited.add(nextUrl);
         return nextUrl;
     }
-
 }
--- WebCrawler/src/com/mackleapes/crawler/SpiderLeg.java
+++ WebCrawler/src/com/mackleapes/crawler/SpiderLeg.java
@@ -6,6 +6,8 @@
 package com.mackleapes.crawler;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -32,29 +34,49 @@ public class SpiderLeg {
      * @return whether or not the crawl was successful
      */
     public boolean crawl(String url) {
+
+        System.out.println("BATATA 2");
+        Connection connection = null;
+        Document htmlDocument = null;
+
+        Proxy proxy = new Proxy(
+                Proxy.Type.HTTP,
+                InetSocketAddress.createUnresolved("172.16.0.10", 3128)
+        );
+
         try {
-            Connection connection = Jsoup.connect(url).userAgent(USER_AGENT);
-            Document htmlDocument = connection.get();
-            this.htmlDocument = htmlDocument;
-            if (connection.response().statusCode() == 200) // 200 is the HTTP OK status code
-            // indicating that everything is great.
-            {
-                System.out.println("\n**Visiting** Received web page at " + url);
-            }
-            if (!connection.response().contentType().contains("text/html")) {
-                System.out.println("**Failure** Retrieved something other than HTML");
-                return false;
-            }
-            Elements linksOnPage = htmlDocument.select("a[href]");
-            System.out.println("Found (" + linksOnPage.size() + ") links");
-            for (Element link : linksOnPage) {
-                this.links.add(link.absUrl("href"));
-            }
-            return true;
-        } catch (IOException ioe) {
-            // We were not successful in our HTTP request
+            connection = Jsoup.connect(url).userAgent(USER_AGENT).proxy(proxy);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
             return false;
         }
+
+        try {
+            htmlDocument = connection.get();
+        } catch (Exception ex) {
+            System.out.println("lol3");
+            ex.printStackTrace();
+            return false;
+        }
+
+        this.htmlDocument = htmlDocument;
+        System.out.println(this.htmlDocument);
+        if (connection.response().statusCode() == 200) // 200 is the HTTP OK status code
+        // indicating that everything is great.
+        {
+            System.out.println("\n**Visiting** Received web page at " + url);
+        }
+        if (!connection.response().contentType().contains("text/html")) {
+            System.out.println("**Failure** Retrieved something other than HTML");
+            return false;
+        }
+        Elements linksOnPage = htmlDocument.select("a[href]");
+        System.out.println("Found (" + linksOnPage.size() + ") links");
+        for (Element link : linksOnPage) {
+            this.links.add(link.absUrl("href"));
+        }
+        return true;
+
     }
 
     /**
